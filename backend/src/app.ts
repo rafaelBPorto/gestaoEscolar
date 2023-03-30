8b1df8958fb796b9eddf87aea05240ac9dc10354
rafaelBporto <rafaelbporto88@gmail.com>
2023-03-30 18:27:06 -0300

    chore: console.log()

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import express, {Express, Request, Response } from 'express';
 import cors from 'cors';
-import { connectDb } from '@/config';
+import { connectDb, loadEnv } from '@/config';
 import { coursesProgramsRouter } from '@/routers/coursesPrograms-router';
 import { coursesRouter } from '@/routers/courses-router';
 
-console.log(process.env.PORT)
+console.log("PORT deveria ser undefined pois o dotenv não foi selecionado ainda porém está sendo PORT=", process.env.PORT)
+loadEnv();
 const app = express();
 
 app
@@ -16,7 +17,6 @@ app
   
 
 export function init(): Promise<Express> {
-  console.log('init')
   connectDb()
   return Promise.resolve(app);
 }
