1837afc601697a1d22b40ef58fd04cc8bd0bcdde
rafaelBporto <rafaelbporto88@gmail.com>
2023-03-31 14:14:04 -0300

    refat: courses-repository

--- backend/src/repositories/courses-repository/index.ts
+++ backend/src/repositories/courses-repository/index.ts
@@ -2,37 +2,25 @@ import { prisma } from '@/config';
 import { Courses, Prisma } from '@prisma/client';
 
 async function getCourses(): Promise<Courses[]> {
-  try {
-    const listCourses: Promise<Courses[]> = prisma.courses.findMany();
-    return listCourses;
-
-  } catch (error) {
-    console.log(error);
-  }
+  return await prisma.courses.findMany();
 };
 
 async function getFindUniqueCourse(courseId: number): Promise<Courses> {
-  try {
-    return prisma.courses.findUnique({
-      where: {
-        id: courseId
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
+  return await prisma.courses.findUnique({
+    where: {
+      id: courseId
+    }
+  });
+}
 
-async function upsertCourse(course: Prisma.CoursesCreateInput, courseId?: number) {
-  const upsert = await prisma.courses.upsert({
+async function upsertCourse(course: Prisma.CoursesCreateInput, courseId?: number): Promise<Courses> {
+  return await prisma.courses.upsert({
     where: {
       id: courseId | 0
     },
     create: { ...course },
     update: { ...course }
   });
-
-  return upsert;
 }
 
 const coursesRepository = {
