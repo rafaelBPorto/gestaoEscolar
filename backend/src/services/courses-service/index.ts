21a24497243c00c1b3af9a1be79a1ac92aed1b10
rafaelBporto <rafaelbporto88@gmail.com>
2023-03-31 14:27:31 -0300

    feat: add error handling to Courses service

--- backend/src/services/courses-service/index.ts
+++ backend/src/services/courses-service/index.ts
@@ -3,14 +3,26 @@ import { Courses, Prisma } from '@prisma/client';
 
 async function getCourses(courseId?: number): Promise<Courses | Courses[]> {
   if (!courseId) {
-    return await coursesRepository.getCourses() as Courses[];
+    const courses = await coursesRepository.getCourses() as Courses[];
+    if (courses.length === 0) {
+      throw new Error('No courses were found');
+    }
+    return courses;
+  } else {
+    const course = await coursesRepository.getFindUniqueCourse(courseId) as Courses;
+    if (!course) {
+      throw new Error(`Course with id ${courseId} not found`);
+    }
+    return course;
   }
-
-  return await coursesRepository.getFindUniqueCourse(courseId) as Courses;
 }
 
 async function upsertCourse(course: Prisma.CoursesCreateInput, id?: number | undefined): Promise<Courses> {
-  return await coursesRepository.upsertCourse(course, id);
+  const result = await coursesRepository.upsertCourse(course, id);
+  if (result == null) {
+    throw new Error("Couldn't update or create the course");
+  }
+  return result;
 }
 
 const coursesService = {
