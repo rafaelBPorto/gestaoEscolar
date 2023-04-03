86baf9acf94760b34cca2583b69504f346e67eca
rafaelBporto <rafaelbporto88@gmail.com>
2023-04-03 14:51:42 -0300

    feat: add validateBody at post /programs router

--- backend/src/routers/programs-router/index.ts
+++ backend/src/routers/programs-router/index.ts
@@ -1,11 +1,13 @@
 import { getPrograms, upsertPrograms } from "@/controllers/programs-controller";
+import { validateBody } from "@/middlewares";
+import { createProgramsSchema } from "@/schemas/programs-schemas";
 import { Router } from "express";
 
 const programsRouter = Router();
 
 programsRouter
   .get('/:id?', getPrograms)
-  .post('/:id?', upsertPrograms);
+  .post('/:id?', validateBody(createProgramsSchema), upsertPrograms);
 
 export { programsRouter };
 
