9081ffdac17451d89ca56582931a6a101c2b83c0
rafaelBporto <rafaelbporto88@gmail.com>
2023-04-03 14:07:42 -0300

    create validation-middleware

---
+++ backend/src/middlewares/validation-middleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { ObjectSchema } from "joi";
+
+export function validateBody<T>(shema: ObjectSchema<T>): ValidationMiddleware {
+  return validate(shema, "body");
+};
+
+export function validateParams<T>(schema: ObjectSchema<T>): ValidationMiddleware {
+  return validate(schema, "params");
+}
+
+function validate(schema: ObjectSchema, type: "body" | "params") {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req[type], {
+      abortEarly: false,
+    });
+
+    if (!error) {
+      next();
+    } else {
+      const errors = error.details.map(detail => detail.message);
+      return res.status(400).send({ message: errors });
+    }
+  };
+}
+
+type ValidationMiddleware = (req: Request, res: Response, next: NextFunction) => void;
