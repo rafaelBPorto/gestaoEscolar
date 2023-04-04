7d8d7c1745d8e31357075be9605756fddf6fd081
rafaelBporto <rafaelbporto88@gmail.com>
2023-04-04 10:53:13 -0300

    feat: function validateBody Aceppt ArraySchema

--- backend/src/middlewares/validation-middleware.ts
+++ backend/src/middlewares/validation-middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import { ObjectSchema } from "joi";
+import { ArraySchema, ObjectSchema } from "joi";
 
-export function validateBody<T>(shema: ObjectSchema<T>): ValidationMiddleware {
+export function validateBody<T>(shema: ObjectSchema<T> | ArraySchema): ValidationMiddleware {
   return validate(shema, "body");
 };
 
@@ -9,7 +9,7 @@ export function validateParams<T>(schema: ObjectSchema<T>): ValidationMiddleware
   return validate(schema, "params");
 }
 
-function validate(schema: ObjectSchema, type: "body" | "params") {
+function validate(schema: ObjectSchema | ArraySchema, type: "body" | "params") {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req[type], {
       abortEarly: false,
