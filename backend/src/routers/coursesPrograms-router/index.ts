37d75f6a4cb24ace19a372690cb446c6c325fa7a
rafaelBporto <rafaelbporto88@gmail.com>
2023-04-04 11:05:48 -0300

    feat: created router postCreateManyCoursesPrograms

--- backend/src/routers/coursesPrograms-router/index.ts
+++ backend/src/routers/coursesPrograms-router/index.ts
@@ -1,9 +1,13 @@
-import { getCoursesPrograms } from '@/controllers/coursesPrograms-controller';
+import { getCoursesPrograms, postManyCoursesProgramas } from '@/controllers/coursesPrograms-controller';
+import { validateBody } from '@/middlewares';
+import { courseProgramsArraySchema } from '@/schemas/coursesPrograms-schema';
 import { Router } from 'express';
 
 const coursesProgramsRouter = Router();
 
 coursesProgramsRouter
-  .get('/:id?', getCoursesPrograms);
+  .get('/:id?', getCoursesPrograms)
+  .post("/", validateBody(courseProgramsArraySchema), postManyCoursesProgramas);
+
 
 export { coursesProgramsRouter };
