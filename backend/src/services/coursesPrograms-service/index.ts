87896357c431731a93d4cd8b8eb978e399531a42
rafaelBporto <rafaelbporto88@gmail.com>
2023-04-04 10:32:40 -0300

    feat: service to createMany coursesPrograms

--- backend/src/services/coursesPrograms-service/index.ts
+++ backend/src/services/coursesPrograms-service/index.ts
@@ -1,3 +1,4 @@
+import { invalidDataError } from '@/errors';
 import coursesProgramsRepository from '@/repositories/coursesPrograms-repository';
 import { CoursesPrograms } from '@prisma/client';
 
@@ -9,8 +10,12 @@ async function getCoursesPrograms(coursesProgramsId?:number): Promise<CoursesPro
   }
 }
 
+async function postCreateManyCoursesPrograms(coursesPrograms:CoursesPrograms[]) {
+  return await coursesProgramsRepository.postManyCoursesProgramas(coursesPrograms);  
+}
 const coursesProgramsService = {
-  getCoursesPrograms
+  getCoursesPrograms,
+  postCreateManyCoursesPrograms
 };
 
 export default coursesProgramsService;
