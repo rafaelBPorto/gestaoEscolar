2e515de8bf751f7c25ac04331bd7eecd84016a7e
rafaelBporto <rafaelbporto88@gmail.com>
2023-04-04 11:02:28 -0300

    fix: courseProgramsArraySchema need min 1 object

--- backend/src/schemas/courses-schemas.ts
+++ backend/src/schemas/courses-schemas.ts
@@ -1,9 +1,10 @@
 import { Prisma } from "@prisma/client";
 import Joi from "joi";
 
-export const createCoursesSchema = Joi.object<Prisma.CoursesCreateInput>({
+export const courseProgramsSchema = Joi.object<Prisma.CoursesProgramsCreateManyInput>({
   name: Joi.string().required(),
-  courseCode: Joi.string().required(),
-  workload: Joi.number().required(),
-  fieldStudy: Joi.string().required(),
+  idCourse: Joi.number().integer().required(),
+  idProgram: Joi.number().integer().required(),
 });
+
+export const courseProgramsArraySchema = Joi.array().items(courseProgramsSchema).min(1);
