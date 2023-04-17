998e304cca89559fd6bdd7e7cc03897ffe2fc349
rafaelBporto <rafaelbporto88@gmail.com>
2023-04-17 14:11:31 -0300

    refact: seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
+import { type } from 'os';
 dotenv.config();
 console.log(process.env.PORT);
 
@@ -7,63 +8,119 @@ const prisma = new PrismaClient();
 
 async function main() {
 
-  await prisma.$executeRaw`TRUNCATE TABLE "Timetable" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "timetable" RESTART IDENTITY CASCADE;`;
   await prisma.$executeRaw`TRUNCATE TABLE "non-school-days" RESTART IDENTITY CASCADE;`;
   await prisma.$executeRaw`TRUNCATE TABLE "courses_programs" RESTART IDENTITY CASCADE;`;
   await prisma.$executeRaw`TRUNCATE TABLE "program" RESTART IDENTITY CASCADE;`;
   await prisma.$executeRaw`TRUNCATE TABLE "course" RESTART IDENTITY CASCADE;`;
-  
-  // Inserir registros na tabela Courses
+
   await prisma.courses.createMany({
     data: [
       {
-        name: 'Matemática',
-        courseCode: 'MAT01',
-        workload: 60,
-        fieldStudy: 'Ciências Exatas',
+        name: 'Aplicações Front-end',
+        courseCode: 'APFE01',
+        workload: 120,
+        fieldStudy: 'Técnoligia da Informação',
       },
       {
-        name: 'Português',
-        courseCode: 'POR01',
-        workload: 45,
-        fieldStudy: 'Linguística',
+        name: 'Single Page Applications',
+        courseCode: 'SPA01',
+        workload: 120,
+        fieldStudy: 'Técnoligia da Informação',
       },
-      // Adicione outros registros aqui, se necessário
+      {
+        name: 'TypeScript',
+        courseCode: 'TS01',
+        workload: 60,
+        fieldStudy: 'Técnoligia da Informação',
+      },
+      {
+        name: 'Prsima',
+        courseCode: 'DB02',
+        workload: 60,
+        fieldStudy: 'Técnoligia da Informação',
+      },
     ],
   });
 
-  // Inserir registros na tabela Programs
   await prisma.programs.createMany({
     data: [
       {
-        name: 'Engenharia de Computação',
-        fieldStudy: 'Ciências Exatas',
+        name: 'Introdução a React',
+        fieldStudy: 'Técnoligia da Informação',
       },
       {
-        name: 'Letras',
-        fieldStudy: 'Linguística',
+        name: 'Back-end',
+        fieldStudy: 'Técnoligia da Informação',
       },
-      // Adicione outros registros aqui, se necessário
     ],
   });
 
-  // Inserir registros na tabela CoursesPrograms
   await prisma.coursesPrograms.createMany({
     data: [
       {
-        name: 'Matemática - Engenharia de Computação',
+        name: 'React - Aplicações Front-end',
         idCourse: 1,
         idProgram: 1,
       },
       {
-        name: 'Português - Letras',
+        name: 'Reac - Single Page Applications',
         idCourse: 2,
-        idProgram: 2,
+        idProgram: 1,
       },
-      // Adicione outros registros aqui, se necessário
     ],
   });
+
+  await prisma.nonSchoolDays.createMany({
+    data: [
+      {
+        date: new Date('2023-01-01'),
+        type: 'holiday',
+        description: 'feriado nacional',
+        shift: 'Morning'
+      },
+      {
+        date: new Date('2023-02-20'),
+        type: 'holiday',
+        description: 'Carnaval',
+        shift: 'Morning'
+      },
+      {
+        date: new Date('2023-02-21'),
+        type: 'holiday',
+        description: 'Carnaval',
+        shift: 'Morning'
+      },
+      {
+        date: new Date('2023-02-22'),
+        type: 'holiday',
+        description: 'Carnaval',
+        shift: 'Morning'
+      },
+      {
+        date: new Date('2023-04-07'),
+        type: 'holiday',
+        description: 'Paixão de Cristo',
+        shift: 'Morning'
+      },
+      {
+        date: new Date('2023-04-21'),
+        type: 'holiday',
+        description: 'Tiradentes',
+        shift: 'Morning'
+      },
+      {
+        date: new Date('2023-05-01'),
+        type: 'holiday',
+        description: 'Dia Mundial do Trabalho',
+        shift: 'Morning'
+      }
+    ]
+
+  });
 }
+
+
 
 main()
   .catch((e) => {
